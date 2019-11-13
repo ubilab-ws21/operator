ec0fca44a976670161ac0d7f4e28c2c0ce17916f
Simon Moser <mosers@tf.uni-freiburg.de>
2019-11-13 13:49:50 +0100

    UI angefangen

---
+++ UI/UI.py
@@ -0,0 +1,25 @@
+from http import server
+from urllib import parse
+
+
+class UIRequestHandler (server.BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == "index.html":
+            pass
+        elif self.path.startswith("getstate"):
+            
+
+    def do_POST(self):
+        if self.path.startswith("getstate"):
+            length = int(self.headers.getheader('content-length'))
+            field_data = self.rfile.read(length)
+            fields = parse.parse_qs(field_data)
+
+
+if __name__ == "__main__":
+    uiserver = server.HTTPServer(("127.0.0.1", 80), UIRequestHandler)
+    try:
+        uiserver.serve_forever()
+        print("Server started")
+    except KeyboardInterrupt:
+        print("Server stopped")
