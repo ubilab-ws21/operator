fa4a63d5e8939a44052fba85e45f94bd5243c48d
Simon Moser <mosers@tf.uni-freiburg.de>
2020-01-19 19:10:09 +0100

    Gametime topic in UI changed, JS refactored and comments added

--- webinterface/script.js
+++ webinterface/script.js
@@ -1,29 +1,53 @@
+// Debug client
 let mqtt;
+// Control client
 let mqttControl;
 let reconnectTimeout = 2000;
 let host = "10.0.0.2";
 let port = 9001;
 
+/**
+ * Short version of getElementById
+ * @param id
+ * @returns {HTMLElement}
+ */
 function getID(id) {
     return document.getElementById(id);
 }
 
+/**
+ * Is called when the connection fails for the debug client
+ * Tries to restart the client connection
+ * @param message
+ */
 function onFailure(message) {
     console.log("Connection attempt for debug failed");
     console.log(message);
     setTimeout(MQTTconnect, reconnectTimeout);
 }
 
+/**
+ * Is called when the connection fails for the control client
+ * Tries to restart the client connection
+ * @param message
+ */
 function onFailureControl(message) {
     console.log("Connection attempt for control failed");
     console.log(message);
     setTimeout(MQTTconnectControl, reconnectTimeout);
 }
 
+/**
+ * Is called when the debug client connected
+ */
 function onConnect() {
     console.log("Connected debug");
 }
 
+/**
+ * Is called when the control client connected
+ * Subscribes to the topic of all groups
+ */
 function onConnectControl() {
     console.log("Connected control");
     for (let i = 1; i < 9; i++) {
@@ -31,6 +55,11 @@ function onConnectControl() {
     }
 }
 
+/**
+ * Is called when the debug client gets a message
+ * Prints the message to the textarea
+ * @param msg
+ */
 function onMessageArrived(msg) {
     let op = getID("output");
     op.value += "Topic " + msg.destinationName + "; time ";
@@ -39,10 +68,18 @@ function onMessageArrived(msg) {
     op.scrollTop = op.scrollHeight;
 }
 
+/**
+ * Is called when the control client gets a message
+ * Performs different actions
+ * @param msg
+ */
 function onMessageArrivedControl(msg) {
-    if (msg.destinationName === "1/gameTime") {
+    // Displays game time
+    if (msg.destinationName === "1/gameTime_formatted") {
         getID("time").innerText = msg.payloadString;
-    } else if (msg.destinationName === "1/gameControl") {
+    }
+    // Dis-/enables game controls
+    else if (msg.destinationName === "1/gameControl") {
         switch (msg.payloadString) {
             case "start":
                 getID("start").disabled = true;
@@ -61,6 +98,7 @@ function onMessageArrivedControl(msg) {
                 break;
         }
     }
+    // Parse JSON and display puzzle status
     try {
         let obj = JSON.parse(msg.payloadString);
         if (obj.method.toLowerCase() === "status") {
@@ -69,65 +107,32 @@ function onMessageArrivedControl(msg) {
 
             // Create the status box if it doesn't exist yet
             if (getID(dst_b64) == null) {
-                // Create dropdown options
-                let opt1 = document.createElement("option");
-                opt1.innerText = "inactive";
-                opt1.value = "inactive";
-                let opt2 = document.createElement("option");
-                opt2.innerText = "active";
-                opt2.value = "active";
-                let opt3 = document.createElement("option");
-                opt3.innerText = "solved";
-                opt3.value = "solved";
-                let opt4 = document.createElement("option");
-                opt4.innerText = "failed";
-                opt4.value = "failed";
-                // Create dropdown field and append options
-                let select = document.createElement("select");
-                select.id = dst_b64 + "_state";
-                select.appendChild(opt1);
-                select.appendChild(opt2);
-                select.appendChild(opt3);
-                select.appendChild(opt4);
-                // Create data field
-                let data = document.createElement("input");
-                data.type = "text";
-                data.id = dst_b64 + "_data";
-                data.readOnly = true;
-                // Create button
-                let button = document.createElement("button");
-                button.innerText = "Change";
-                button.type = "button";
-                button.setAttribute("onclick", "changeState('" + dst_b64 + "');");
-                // Create form and append fields
-                let form = document.createElement("form");
-                form.appendChild(select);
-                form.appendChild(document.createElement("br"));
-                form.appendChild(data);
-                form.appendChild(document.createElement("br"));
-                form.appendChild(button);
-                // Create title/topic name
-                let header = document.createElement("b");
-                header.innerText = dst;
-                // Create container and append elements
-                let ele = document.createElement("div");
-                ele.id = dst_b64;
-                ele.className = "control";
-                ele.appendChild(header);
-                ele.appendChild(form);
-                // Select group container and append single container
-                let cont = getID("c" + dst.charAt(0));
-                cont.appendChild(ele);
+                getID("c" + dst.charAt(0)).innerHTML += `<div id="${dst_b64}" class="control"><b>${dst}</b><form>
+                    <select id="${dst_b64}_state">
+                        <option value="inactive">inactive</option>
+                        <option value="active">active</option>
+                        <option value="solved">solved</option>
+                        <option value="failed">failed</option>
+                    </select><br><input type="text" id="${dst_b64}_data" readonly=""><br>
+                    <button type="button" onclick="changeState('${dst_b64}');">Change</button></form></div>`;
             }
 
             // Change the state of the status box
-            getID(dst_b64 + "_state").value = obj.state.toLowerCase;
+            getID(dst_b64 + "_state").value = obj.state.toLowerCase();
             getID(dst_b64 + "_data").value = obj.data || "";
         }
-    } catch {
+    } catch(error){
+        // Catch only JSON syntax error
+        if(!(error instanceof SyntaxError)) {
+            throw error;
+        }
     }
 }
 
+/**
+ * Creates and connects the debug client
+ * @constructor
+ */
 function MQTTconnect() {
     console.log("Connecting debug to " + host + ":" + port + "...");
     mqtt = new Paho.Client(host, port, "", "ws-client-d" + ~~(Date.now() / 1000));
@@ -135,6 +140,10 @@ function MQTTconnect() {
     mqtt.connect({timeout: 3, onSuccess: onConnect, onFailure: onFailure});
 }
 
+/**
+ * Creates and connects the control client
+ * @constructor
+ */
 function MQTTconnectControl() {
     console.log("Connecting control to " + host + ":" + port + "...");
     mqttControl = new Paho.Client(host, port, "", "ws-client-c" + ~~(Date.now() / 1000));
@@ -142,6 +151,11 @@ function MQTTconnectControl() {
     mqttControl.connect({timeout: 3, onSuccess: onConnectControl, onFailure: onFailureControl});
 }
 
+/**
+ * Toggles the subscription button for a specific topic
+ * @param topic
+ * @param button
+ */
 function toggle(topic, button) {
     if (button.parentElement.id === "soff") {
         mqtt.subscribe(topic + "/#");
@@ -151,12 +165,19 @@ function toggle(topic, button) {
     getID(button.parentElement.id === "soff" ? "son" : "soff").appendChild(button);
 }
 
+/**
+ * Toggles all un- or subscription buttons by calling their click event
+ * @param from
+ */
 function toggleAll(from) {
     while (getID(from).firstElementChild !== null) {
         getID(from).firstElementChild.click();
     }
 }
 
+/**
+ * Sends the message entered into the form
+ */
 function send() {
     if (getID("send_topic").value === "") {
         alert("Topic is required");
@@ -168,6 +189,10 @@ function send() {
     getID("send_retain").checked = false;
 }
 
+/**
+ * Toggles a given help block
+ * @param n
+ */
 function toggleHelp(n) {
     let id = n === 1 ? "help" : "help2";
     if (getID(id).style.display === "none") {
@@ -177,6 +202,9 @@ function toggleHelp(n) {
     }
 }
 
+/**
+ * Sends a message to the camera control to change the active camera in fallback mode
+ */
 function changeCamera() {
     let xhr = new XMLHttpRequest();
     let number = getID("camera-selection").value;
@@ -189,6 +217,10 @@ function changeCamera() {
     xhr.send();
 }
 
+/**
+ * Changes the state of a puzzle displayed in the control section
+ * @param dst_b64
+ */
 function changeState(dst_b64) {
     let dst = atob(dst_b64);
     let state = getID(dst_b64 + "_state").value;
@@ -196,6 +228,10 @@ function changeState(dst_b64) {
     mqttControl.send(dst, json_obj, 2);
 }
 
+/**
+ * Changes the currently active tab
+ * @param target
+ */
 function changeTab(target) {
     for (name of ["control", "cameras", "cameras-fallback", "mosquitto"]) {
         if (name !== target) {
@@ -208,14 +244,25 @@ function changeTab(target) {
     }
 }
 
+/**
+ * Sends a game control command
+ * @param content
+ */
 function command(content) {
     mqttControl.send("1/gameControl", content, 2, true);
 }
 
+/**
+ * Sends a environment control command
+ */
 function envSet() {
     mqttControl.send(getID("env-target").value, getID("env-command").value + getID("env-number1").value);
 }
 
+/**
+ * En-/disables valid environment commands upon selection of topic
+ * @param select
+ */
 function validateCommands(select) {
     let cmd = getID("env-command");
     cmd.disabled = false;
@@ -244,6 +291,10 @@ function validateCommands(select) {
     }
 }
 
+/**
+ * En-/disables valid environment command values upon selection of command
+ * @param select
+ */
 function validateNumbers(select) {
     let num = getID("env-number1");
     num.disabled = false;
