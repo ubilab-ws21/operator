5c4d25d3f463fc6a9723536e4b2cbf0f0b15c953
CuddlySheep <rebmann@informatik.uni-freiburg.de>
2020-01-22 11:41:26 +0100

    Generalize workflows by introducing BaseWorkflow

--- logic/workflow.py
+++ logic/workflow.py
@@ -3,14 +3,12 @@ from message import Message, Method, State, fromJSON
 from enum import Enum
 
 
-class Workflow:
+class BaseWorkflow:
     """
-    This class represents the default workflow implementation of a puzzle,
-    defined in following UML sequence diagram:
-    https://github.com/ubilab-escape/operator/blob/master/doc/design/general_%CE%BCC_workflow.svg
+    This class provides the basic structure and functionality for workflows.
     """
 
-    def __init__(self, name, topic, settings=None):
+    def __init__(self, name, settings=None):
         """
         Initializes a new instance of this class.
 
@@ -19,13 +17,9 @@ class Workflow:
         name : str
             Display name of the workflow.
 
-        topic : str
-            Name of the MQTT topic.
-
         settings: keywords
             An dictionary of global settings.
         """
-        self.topic = topic
         self.name = name
         self.settings = settings
         self._on_workflow_failed = None
@@ -44,12 +38,6 @@ class Workflow:
         client : Client
             MQTT client
         """
-        data = self.getData(self.settings)
-        message = Message(Method.TRIGGER, State.ON, data)
-        client.publish(self.topic, message.toJSON())
-        print("[%s] Started..." % (self.name))
-        client.subscribe(self.topic)
-        print("[%s] Subscribed to topic '%s'..." % (self.name, self.topic))
         self.activated = True
 
     def dispose(self, client):
@@ -61,91 +49,19 @@ class Workflow:
         client : Client
             MQTT client
         """
-        client.unsubscribe(self.topic)
-        print("[%s] Unsubscribed from topic '%s'..." % (self.name, self.topic))
         self.activated = False
 
-    def getData(self, settings):
-        data = None
-
-        if settings:
-            if len(settings) == 1:
-                data = next(iter(settings.values()))
-            else:
-                data = json.dumps(settings)
-        return data
-
     def on_message(self, msg):
-        """
-        Processes the message sended by the MQTT server.
+        pass
 
-        Parameters
-        ----------
-        msg : Message
-            Message from the MQTT topic.
-        """
-        try:
-            # Check for relevant topic
-            if msg.topic != self.topic:
-                return
-
-            message = msg.payload.decode("utf-8")
-            obj = fromJSON(message)
-            if obj.method == Method.STATUS:
-                print("[%s] State change to '%s'"
-                      % (self.name, obj.state.name))
-                if obj.state == State.INACTIVE:
-                    self.on_received_status_inactive(obj.data)
-                elif obj.state == State.ACTIVE:
-                    self.on_received_status_active(obj.data)
-                elif obj.state == State.SOLVED:
-                    self.on_received_status_finished(obj.data)
-                elif obj.state == State.FAILED:
-                    self.on_received_status_failed(obj.data)
-                else:
-                    if self._on_workflow_failed:
-                        self._on_workflow_failed(
-                            self.name,
-                            "[%s] State '%s' is not supported"
-                            % (self.name, obj.state))
-            elif obj.method == Method.TRIGGER:
-                print("[%s] Requested trigger '%s'"
-                      % (self.name, obj.state.name))
-                if obj.state == State.ON:
-                    self.on_received_trigger_on(obj.data)
-                elif obj.state == State.OFF:
-                    self.on_received_trigger_off(obj.data)
-                else:
-                    if self._on_workflow_failed:
-                        self._on_workflow_failed(
-                            self.name,
-                            "[%s] Trigger state '%s' is not supported"
-                            % (self.name, obj.state))
-            elif obj.method == Method.MESSAGE:
-                print("[%s] Received message with method 'MESSAGE'. "
-                      "Nothing to do..." % (self.name))
+    def getSettings(self):
+        data = None
+        if self.settings:
+            if len(self.settings) == 1:
+                data = next(iter(self.settings.values()))
             else:
-                if self._on_workflow_failed:
-                    self._on_workflow_failed(
-                        self.name,
-                        "[%s] Method '%s' is not supported"
-                        % (self.name, obj.method))
-        except Exception as e:
-            if self._on_workflow_failed:
-                self._on_workflow_failed(
-                    self.name,
-                    "[%s] No valid JSON: %s" % (self.name, str(e)))
-
-    def toJSON(self):
-        """
-        Generates a JSON of the current state of the workflow.
-        """
-        return json.dumps({
-            'name': self.name,
-            'type': self.type,
-            'activated': self.activated,
-            'finished': self.finished
-        })
+                data = json.dumps(self.settings)
+        return data
 
     def register_on_failed(self, func):
         """
@@ -169,36 +85,33 @@ class Workflow:
         """
         self._on_workflow_finished = func
 
-    def on_received_status_inactive(self, data):
-        print("  ==> Nothing to do")
+    def toJSON(self):
+        """
+        Generates a JSON of the current state of the workflow.
+        """
+        stateDict = {
+            'name': self.name,
+            'type': self.type,
+            'activated': self.activated,
+            'finished': self.finished
+        }
 
-    def on_received_status_active(self, data):
-        print("  ==> Nothing to do")
+        self._addState(stateDict)
 
-    def on_received_status_finished(self, data):
-        print("  ==> Puzzle solved successfully")
-        if self._on_workflow_finished:
-            self._on_workflow_finished(self.name)
-        self.finished = True
+        return json.dumps(stateDict)
 
-    def on_received_status_failed(self, data):
-        print("  ==> An error occured: %s" % (data))
-        if self._on_workflow_failed:
-            self._on_workflow_failed(self.name, data)
-
-    def on_received_trigger_on(self, data):
-        print("  ==> Nothing to do")
-
-    def on_received_trigger_off(self, data):
-        print("  ==> Nothing to do")
+    def _addState(self, stateDict):
+        pass
 
 
-class SequenceWorkflow(Workflow):
+class Workflow(BaseWorkflow):
     """
-    This class implements a wrapper to run multiple workflows in sequence.
+    This class represents the default workflow implementation of a puzzle,
+    defined in following UML sequence diagram:
+    https://github.com/ubilab-escape/operator/blob/master/doc/design/general_%CE%BCC_workflow.svg
     """
 
-    def __init__(self, name, workflows):
+    def __init__(self, name, topic, settings=None):
         """
         Initializes a new instance of this class.
 
@@ -206,12 +119,167 @@ class SequenceWorkflow(Workflow):
         ----------
         name : str
             Display name of the workflow.
+
         topic : str
             Name of the MQTT topic.
+
+        settings: keywords
+            An dictionary of global settings.
+        """
+        super().__init__(name, settings)
+        self.topic = topic
+
+    def execute(self, client):
+        """
+        Executes this workflow.
+
+        Parameters
+        ----------
+        client : Client
+            MQTT client
+        """
+        self._publishTriggerOn(client)
+        self._subscripeToTopic(client)
+        super().execute(client)
+
+    def dispose(self, client):
+        """
+        Disposes this workflow.
+
+        Parameters
+        ----------
+        client : Client
+            MQTT client
+        """
+        self._unsubscripeFromTopic(client)
+        super().dispose(client)
+
+    def on_message(self, msg):
+        """
+        Processes the message sended by the MQTT server.
+
+        Parameters
+        ----------
+        msg : Message
+            Message from the MQTT topic.
+        """
+        try:
+            # Check for relevant topic
+            if msg.topic != self.topic:
+                return
+
+            message = msg.payload.decode("utf-8")
+            obj = fromJSON(message)
+            if obj.method == Method.STATUS:
+                print("[%s] State change to '%s'"
+                      % (self.name, obj.state.name))
+                if obj.state == State.INACTIVE:
+                    self._on_received_status_inactive(obj.data)
+                elif obj.state == State.ACTIVE:
+                    self._on_received_status_active(obj.data)
+                elif obj.state == State.SOLVED:
+                    self._on_received_status_finished(obj.data)
+                elif obj.state == State.FAILED:
+                    self._on_received_status_failed(obj.data)
+                else:
+                    if self._on_workflow_failed:
+                        self._on_workflow_failed(
+                            self.name,
+                            "[%s] State '%s' is not supported"
+                            % (self.name, obj.state))
+            elif obj.method == Method.TRIGGER:
+                print("[%s] Requested trigger '%s'"
+                      % (self.name, obj.state.name))
+                if obj.state == State.ON:
+                    self._on_received_trigger_on(obj.data)
+                elif obj.state == State.OFF:
+                    self._on_received_trigger_off(obj.data)
+                else:
+                    if self._on_workflow_failed:
+                        self._on_workflow_failed(
+                            self.name,
+                            "[%s] Trigger state '%s' is not supported"
+                            % (self.name, obj.state))
+            elif obj.method == Method.MESSAGE:
+                print("[%s] Received message with method 'MESSAGE'. "
+                      "Nothing to do..." % (self.name))
+            else:
+                if self._on_workflow_failed:
+                    self._on_workflow_failed(
+                        self.name,
+                        "[%s] Method '%s' is not supported"
+                        % (self.name, obj.method))
+        except Exception as e:
+            if self._on_workflow_failed:
+                self._on_workflow_failed(
+                    self.name,
+                    "[%s] No valid JSON: %s" % (self.name, str(e)))
+
+        super().on_message(msg)
+
+    def _publishTriggerOn(self, client):
+        if self.topic is not None:
+            data = self.getSettings()
+            message = Message(Method.TRIGGER, State.ON, data)
+            client.publish(self.topic, message.toJSON())
+            print("[%s] Started..." % (self.name))
+
+    def _subscripeToTopic(self, client):
+        if self.topic is not None:
+            client.subscribe(self.topic)
+            print("[%s] Subscribed to topic '%s'..." % (self.name, self.topic))
+
+    def _unsubscripeFromTopic(self, client):
+        if self.topic is not None:
+            client.unsubscribe(self.topic)
+            print("[%s] Unsubscribed from topic '%s'..."
+                  % (self.name, self.topic))
+
+    def _on_received_status_inactive(self, data):
+        print("  ==> Nothing to do")
+
+    def _on_received_status_active(self, data):
+        print("  ==> Nothing to do")
+
+    def _on_received_status_finished(self, data):
+        print("  ==> Puzzle solved successfully")
+        if self._on_workflow_finished:
+            self._on_workflow_finished(self.name)
+        self.finished = True
+
+    def _on_received_status_failed(self, data):
+        print("  ==> An error occured: %s" % (data))
+        if self._on_workflow_failed:
+            self._on_workflow_failed(self.name, data)
+
+    def _on_received_trigger_on(self, data):
+        print("  ==> Nothing to do")
+
+    def _on_received_trigger_off(self, data):
+        print("  ==> Nothing to do")
+
+
+class SequenceWorkflow(BaseWorkflow):
+    """
+    This class implements a wrapper to run multiple workflows in sequence.
+    """
+
+    def __init__(self, name, workflows, settings=None):
+        """
+        Initializes a new instance of this class.
+
+        Parameters
+        ----------
+        name : str
+            Display name of the workflow.
+
         workflows : Workflow[]
             Collection of workflows should be executed in parallel.
+
+        settings: keywords
+            An dictionary of global settings.
         """
-        super().__init__(name, None)
+        super().__init__(name, settings)
         self.workflows = workflows
         self.client = None
         self.current_workflow = 0
@@ -227,7 +295,7 @@ class SequenceWorkflow(Workflow):
         """
         self.client = client
         self.__subscribeCurrentWorkflow(self.client)
-        self.activated = True
+        super().execute(client)
 
     def dispose(self, client):
         """
@@ -239,7 +307,7 @@ class SequenceWorkflow(Workflow):
             MQTT client
         """
         self.current_workflow = 0
-        self.activated = False
+        super().dispose(client)
 
     def on_message(self, msg):
         """
@@ -253,22 +321,27 @@ class SequenceWorkflow(Workflow):
         if (self.current_workflow < len(self.workflows)):
             workflow = self.workflows[self.current_workflow]
             workflow.on_message(msg)
+        super().on_message(msg)
 
-    def toJSON(self):
+    def _addState(self, stateDict):
         """
         Generates a JSON of the current state of the workflow.
         """
         stateJsons = []
         for workflow in self.workflows:
             stateJsons += [json.loads(workflow.toJSON())]
+        stateDict['workflows'] = stateJsons
+        super()._addState(stateDict)
 
-        return json.dumps({
-            'name': self.name,
-            'type': self.type,
-            'activated': self.activated,
-            'finished': self.finished,
-            'workflows': stateJsons
-        })
+    def __subscribeCurrentWorkflow(self, client):
+        workflow = self.workflows[self.current_workflow]
+        workflow.register_on_failed(self.__on_workflow_failed)
+        workflow.register_on_finished(self.__on_workflow_finished)
+        workflow.execute(client)
+
+    def __unsubscribeCurrentWorkflow(self, client):
+        workflow = self.workflows[self.current_workflow]
+        workflow.dispose(client)
 
     def __on_workflow_failed(self, name, error):
         if self._on_workflow_failed:
@@ -285,25 +358,15 @@ class SequenceWorkflow(Workflow):
         else:
             self.__subscribeCurrentWorkflow(self.client)
 
-    def __subscribeCurrentWorkflow(self, client):
-        workflow = self.workflows[self.current_workflow]
-        workflow.register_on_failed(self.__on_workflow_failed)
-        workflow.register_on_finished(self.__on_workflow_finished)
-        workflow.execute(client)
 
-    def __unsubscribeCurrentWorkflow(self, client):
-        workflow = self.workflows[self.current_workflow]
-        workflow.dispose(client)
-
-
-class ParallelWorkflow(Workflow):
+class ParallelWorkflow(BaseWorkflow):
     """
     This class implements a wrapper to run multiple workflows in parallel.
     The parallel workflow is a composition organising the flow of one or
     more arbitary workflows.
     """
 
-    def __init__(self, name, workflows):
+    def __init__(self, name, workflows, settings=None):
         """
         Initializes a new instance of this class.
 
@@ -311,12 +374,14 @@ class ParallelWorkflow(Workflow):
         ----------
         name : str
             Display name of the workflow.
-        topic : str
-            Name of the MQTT topic.
+
         workflows : Workflow[]
             Collection of workflows should be executed in parallel.
+
+        settings: keywords
+            An dictionary of global settings.
         """
-        super().__init__(name, None)
+        super().__init__(name, settings)
         self.workflows = workflows
         self.workflow_finished = {}
         for workflow in self.workflows:
@@ -337,7 +402,7 @@ class ParallelWorkflow(Workflow):
         print("[%s] Starting in parallel..." % (", ".join(names)))
         for workflow in self.workflows:
             workflow.execute(client)
-        self.activated = True
+        super().execute(client)
 
     def dispose(self, client):
         """
@@ -350,7 +415,7 @@ class ParallelWorkflow(Workflow):
         """
         for workflow in self.workflows:
             workflow.dispose(client)
-        self.activated = False
+        super().dispose(client)
 
     def on_message(self, msg):
         """
@@ -363,22 +428,17 @@ class ParallelWorkflow(Workflow):
         """
         for workflow in self.workflows:
             workflow.on_message(msg)
+        super().on_message(msg)
 
-    def toJSON(self):
+    def _addState(self, stateDict):
         """
         Generates a JSON of the current state of the workflow.
         """
         stateJsons = []
         for workflow in self.workflows:
             stateJsons += [json.loads(workflow.toJSON())]
-
-        return json.dumps({
-            'name': self.name,
-            'type': self.type,
-            'activated': self.activated,
-            'finished': self.finished,
-            'workflows': stateJsons
-        })
+        stateDict['workflows'] = stateJsons
+        super()._addState(stateDict)
 
     def __on_workflow_failed(self, name, error):
         if self._on_workflow_failed:
@@ -429,6 +489,7 @@ class DoorWorkflow(Workflow):
 
 
 class ActivateLaserWorkflow(Workflow):
+
     def execute(self, client):
         """
         Executes this workflow.
@@ -438,12 +499,8 @@ class ActivateLaserWorkflow(Workflow):
         client : Client
             MQTT client
         """
-        data = self.getData(self.settings)
-        message = Message(Method.TRIGGER, State.ON, data)
-        client.publish(self.topic, message.toJSON())
-        print("[%s] Started..." % (self.name))
+        super().execute(client)
 
-        print("[%s] Laser is activated..." % (self.name))
         if self._on_workflow_finished:
             self._on_workflow_finished(self.name)
         self.finished = True
