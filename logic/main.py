b0889b309a6177664fad8467b858007adbd482f1
CuddlySheep <rebmann@informatik.uni-freiburg.de>
2020-02-04 14:10:43 +0100

    [close #53] Add MQTT IP to command line args

--- logic/main.py
+++ logic/main.py
@@ -5,14 +5,33 @@ import argparse
 from workflow_controller import WorkflowController
 
 
-def loadWorkflow(module_name, class_name):
+def load_workflow(module_name, class_name):
+    """
+    Loads the dynamically on runtime.
+    Can be interpreted as:
+    from <module_name> import <class_name>
+
+    Parameters
+    ----------
+    module_name : str
+        The module name is the name of the .py file to be loaded from.
+
+    class_name : str
+        The class which should be imported from the module
+
+    Returns
+    -------
+    The instantiated workflow definition.
+    """
     module = __import__(module_name)
-    mainWorkflow = getattr(module, class_name)
-    return mainWorkflow()
+    main_morkflow = getattr(module, class_name)
+    return main_morkflow()
 
 
-def parseArgs():
-    # initiate the parser
+def parse_args():
+    """
+    Parses the command line arguments.
+    """
     parser = argparse.ArgumentParser()
 
     help_text = """
@@ -20,22 +39,29 @@ def parseArgs():
     Format: module:class
     with get method returning an array of Workflows.
     """
-
-    # add long and short argument
     parser.add_argument("--workflow_def", "-d", help=help_text)
-
-    # read arguments from the command line
+    parser.add_argument(
+        "--mqtt_host",
+        "-m",
+        default="127.0.0.1",
+        help="IP of the MQTT server.")
     return parser.parse_args()
 
 
 def shutdown(controller):
+    """
+    Shutdown the programm.
+    """
     controller.disconnect()
     sys.exit(0)
 
 
 if __name__ == "__main__":
-    args = parseArgs()
+    # parse command line arguments
+    args = parse_args()
+    mqtt_url = args.mqtt_host
 
+    # get parameter to load workflow definition
     workflow_module = "workflow_definition"
     workflow_class = "WorkflowDefinition"
     if args.workflow_def:
@@ -43,11 +69,14 @@ if __name__ == "__main__":
         workflow_module = definition[0]
         workflow_class = definition[1]
 
-    mqtt_url = "127.0.0.1"
+    # load workflow to be executed
+    workflow_factory = load_workflow(workflow_module, workflow_class)
 
-    workflow_factory = loadWorkflow(workflow_module, workflow_class)
+    # create workflow controller with defined workflow
     controller = WorkflowController(mqtt_url, workflow_factory)
     controller.connect()
+
+    # listen to SIGINT and wait until exit request received 
     signal.signal(signal.SIGINT, lambda sig, frame: shutdown(controller))
     print('Press Ctrl+C to exit...')
     signal.pause()
