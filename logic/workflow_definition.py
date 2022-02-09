8c261083fb2b8b791065f7d6ce2cb177e6d532da
Sebastian Böttcher <seb.boett@gmail.com>
2022-02-09 13:37:35 +0100

    updated workflow definition

--- logic/workflow_definition.py
+++ logic/workflow_definition.py
@@ -23,13 +23,9 @@ class WorkflowDefinition:
                 SendTriggerWorkflow("Reset safe", "5/safe/control", State.OFF),
                 SendTriggerWorkflow("Close lab room door", "4/door/entrance", State.OFF),
                 SendTriggerWorkflow("Close server room door", "4/door/server", State.OFF),
-                LightControlWorkflow(Location.LOBBYROOM, State.ON),
-                LightControlWorkflow(Location.MAINROOM, State.ON),
-                LightControlWorkflow(Location.SERVERROOM, State.ON),
-
-                # Puzzle 3 init
-                #SendMessageWorkflow("Reset Radio", "3/gamecontrol", "idle"),
-                #SendMessageWorkflow("Set Radio Mode", "3/touchgame/displayTime", False),
+                LightControlWorkflow(Location.LOBBYROOM, State.ON, 255, (255, 255, 255)),
+                LightControlWorkflow(Location.MAINROOM, State.ON, 255, (255, 255, 255)),
+                LightControlWorkflow(Location.SERVERROOM, State.ON, 255, (255, 255, 255)),
 
                 # Puzzle 5 init
                 SendMessageWorkflow("Set Battery Level", "5/battery/1/level", 0),
@@ -39,29 +35,49 @@ class WorkflowDefinition:
 
 
             SequenceWorkflow("Lobby Room", [
+                Workflow("Power Failure Trigger", "env/powerfail"),
+                CombinedWorkflow("Power Failure Env", [
+                    LightControlWorkflow(Location.LOBBYROOM, State.ON, 255, (0, 0, 0)),
+                    LightControlWorkflow(Location.MAINROOM, State.ON, 255, (0, 0, 0)),
+                    LightControlWorkflow(Location.SERVERROOM, State.ON, 255, (0, 0, 0)),
+                    TTSAudioWorkflow("TTS1", "Warning, power failure! Warning, power failure!"),
+                    LightControlWorkflow(Location.LOBBYROOM, State.ON, 255, (255, 0, 0)),
+                    #SendTriggerWorkflow("Play Video", "env/video", State.ON),
+                ]),
                 SequenceWorkflow("Puzzle 1 - Cube", [
                     Workflow("Panels Released", "1/cube/state"),
                     Workflow("Panels Placed", "1/panel/state"),
                 ]),
                 Workflow("Input Keypad Code", "4/puzzle"),
                 SendTriggerWorkflow("Open Control Room Door", "4/door/entrance", State.ON),
+                CombinedWorkflow("First Door Open Env", [
+                    LightControlWorkflow(Location.MAINROOM, State.ON, 255, (255, 0, 0)),
+                    TTSAudioWorkflow("TTS2", "Warning, emergency backup battery empty, please recharge!"),
+                ]),
             ]),
+
 
             SequenceWorkflow("Control Room", [
                 SequenceWorkflow("Puzzle 5 - Battery", [
                     Workflow("Battery Recharged", "5/control_room/power"),
+                ]),
+                CombinedWorkflow("Backup Power Restored Env", [
+                    LightControlWorkflow(Location.LOBBY, State.ON, 255, (255, 255, 255)),
+                    LightControlWorkflow(Location.MAINROOM, State.ON, 255, (255, 255, 255)),
+                    LightControlWorkflow(Location.SERVERROOM, State.ON, 255, (255, 0, 0)),
+                    TTSAudioWorkflow("TTS3", "Emergency power restored! Generators offline! Restart server!"),
                 ]),
                 SequenceWorkflow("Puzzle 3 - Radio", [
                     Workflow("Antenna Aligned", "3/gamecontrol/antenna"),
                     Workflow("Radio Tuned", "3/gamecontrol/map"),
                     Workflow("Touch Game Solved", "3/gamecontrol/touchgame"),
                 ]),
-                #SequenceWorkflow("Puzzle 2 - Switchboard", [
-                #    Workflow("Switchboard Opened", "0/dummy"),
-                #    Workflow("Switches Correct", "0/dummy"),
-                #]),
+                SequenceWorkflow("Puzzle 2 - Switchboard", [
+                    Workflow("Switchboard Solved", "2/esp"),
+                ]),
                 SendTriggerWorkflow("Open Server Room Door", "4/door/server", State.ON),
             ]),
+
 
             SequenceWorkflow("Server Room", [
                 SequenceWorkflow("Puzzle 4 - Server", [
@@ -71,11 +87,12 @@ class WorkflowDefinition:
 
 
             ExitWorkflow([
-                Workflow("Radio Success", "3/audiocontrol/roomsolved"),
+                SendTriggerWorkflow("Radio Success", "3/audiocontrol/roomsolved", State.ON),
+                #SendTriggerWorkflow("Play Video", "env/video", State.ON),
                 LightControlWorkflow(Location.LOBBYROOM, State.ON, 255, (0, 255, 0)),
                 LightControlWorkflow(Location.MAINROOM, State.ON, 255, (0, 255, 0)),
                 LightControlWorkflow(Location.SERVERROOM, State.ON, 255, (0, 255, 0)),
-                TTSAudioWorkflow("Play success", "success.mp3", True),
+                #TTSAudioWorkflow("Play success", "success.mp3", True),
             ])
         ]
 
